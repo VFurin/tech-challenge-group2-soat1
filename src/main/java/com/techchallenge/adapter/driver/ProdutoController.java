7ae4d441afdc6675d440ce809b9606f63443dd4a
Danilo Monteiro <dmonteiro.souza@gmail.com>
2023-06-14 08:58:35 -0300

    feature: Adicionado docker-compose.yml e refatoração de código

--- src/main/java/com/techchallenge/adapter/driver/ProdutoController.java
+++ src/main/java/com/techchallenge/adapter/driver/ProdutoController.java
@@ -1,16 +1,16 @@
 package com.techchallenge.adapter.driver;
 
 import com.techchallenge.adapter.driver.model.ProdutoModel;
-import com.techchallenge.adapter.driver.model.input.ProdutoInput;
 import com.techchallenge.adapter.mapper.ProdutoMapper;
 import com.techchallenge.core.applications.service.ProdutoService;
 import com.techchallenge.core.domain.Produto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.Collection;
 import java.util.List;
 
@@ -24,14 +24,6 @@ public class ProdutoController {
     @Autowired
     private ProdutoMapper mapper;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ProdutoModel criar(@RequestBody @Valid ProdutoInput input) {
-        Produto produto = mapper.toDomainObject(input);
-        produto = service.salvar(produto);
-
-        return mapper.toModel(produto);
-    }
     @GetMapping
     public Collection<ProdutoModel> listar() {
         List<Produto> todosProdutos = service.buscarTodos();
@@ -42,20 +34,5 @@ public class ProdutoController {
     public Collection<ProdutoModel> listarPorCategoria(@PathVariable String categoria) {
         List<Produto> produto = service.buscarPorCategoria(categoria);
         return mapper.toCollectionModel(produto);
-    }
-
-    @PutMapping(value= "/{produtoId}")
-    public ProdutoModel atualizar(@PathVariable Long produtoId, @RequestBody @Valid ProdutoInput input) {
-        Produto produtoAtual = service.editar(produtoId);
-        mapper.copyToDomainObject(input, produtoAtual);
-        produtoAtual = service.salvar(produtoAtual);
-
-        return mapper.toModel(produtoAtual);
-    }
-
-    @DeleteMapping(value= "/{produtoId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void remover(@PathVariable Long produtoId) {
-        service.excluir(produtoId);
     }
 }
