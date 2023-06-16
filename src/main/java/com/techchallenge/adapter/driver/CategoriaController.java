52474363e4313569d4bfbd81f30739884d5072dc
Marcel Rodrigo Cozono <marcel.cozono@gmail.com>
2023-06-16 12:08:24 -0300

    Ajustes realizados do merge.

--- src/main/java/com/techchallenge/adapter/driver/CategoriaController.java
+++ src/main/java/com/techchallenge/adapter/driver/CategoriaController.java
@@ -1,11 +1,7 @@
 package com.techchallenge.adapter.driver;
 
-import com.techchallenge.adapter.driver.model.CategoriaModel;
-import com.techchallenge.adapter.mapper.CategoriaMapper;
-import com.techchallenge.core.applications.service.CategoriaService;
-import com.techchallenge.core.domain.Categoria;
-
-import io.swagger.annotations.Api;
+import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,12 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-feature/ajustes-model-swagger
-@Api(tags = "Categorias")
-  
-import java.util.Collection;
-import java.util.List;
-main
+import com.techchallenge.adapter.driver.model.CategoriaModel;
+import com.techchallenge.adapter.mapper.CategoriaMapper;
+import com.techchallenge.core.applications.service.CategoriaService;
+import com.techchallenge.core.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CategoriaController {
