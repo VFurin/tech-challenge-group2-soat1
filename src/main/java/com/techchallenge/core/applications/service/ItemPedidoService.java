3ead40c0034388d23dcd2897fdb1fb6fb28d52e0
Marcel Rodrigo Cozono <marcel.cozono@gmail.com>
2023-06-26 20:18:50 -0300

    refactor: Refatoração de código e incrementação do README

--- src/main/java/com/techchallenge/core/applications/service/ItemPedidoService.java
+++ src/main/java/com/techchallenge/core/applications/service/ItemPedidoService.java
@@ -1,8 +1,5 @@
 package com.techchallenge.core.applications.service;
 
-import java.time.OffsetDateTime;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +9,6 @@ import com.techchallenge.core.applications.ports.ProdutoRepository;
 import com.techchallenge.core.domain.ItemPedido;
 import com.techchallenge.core.domain.Pedido;
 import com.techchallenge.core.domain.Produto;
-import com.techchallenge.core.domain.StatusPedido;
-import com.techchallenge.core.domain.exception.NegocioException;
 
 @Service
 public class ItemPedidoService {
@@ -27,30 +22,6 @@ public class ItemPedidoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    public List<Pedido> buscarPedidos() {
-        return pedidoRepository.findAll();
-    }
-
-    public Pedido buscarPedidoPorId(Long id) {
-        return pedidoRepository.findById(id).orElseThrow(() -> new NegocioException(
-                String.format("Não existe pedido com o id %d", id)));
-    }
-
-    public List<Pedido> buscarPedidosPorStatus(StatusPedido statusPedido) {
-        return pedidoRepository.findByStatus(statusPedido);
-    }
-
-    public void atualizarStatusDoPedido(Pedido pedido, StatusPedido statusPedido) {
-        pedido.setStatus(statusPedido);
-        pedidoRepository.save(pedido);
-    }
-    
-    public Pedido inicializar(Pedido pedido) {
-        pedido.setStatus(StatusPedido.GERACAO);
-        pedido.setDataSolicitacao(OffsetDateTime.now());
-        return pedidoRepository.save(pedido);
-    }
-    
     public ItemPedido adicionarItemAoPedido(Long idPedido, ItemPedido itemPedido) {
     	Pedido pedido = pedidoRepository.findById(idPedido).get();
     	Produto produto = produtoRepository.findById(itemPedido.getProduto().getId()).get();
